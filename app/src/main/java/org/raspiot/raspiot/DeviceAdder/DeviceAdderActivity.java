dd09db6bff4386b1c49faa75e14488df485437c5
xjhuang <xjhuang@s-an.org>
2017-11-25 23:22:25 +0800

    Add function:  scan QR code to add device

--- app/src/main/java/org/raspiot/raspiot/DeviceAdder/DeviceAdderActivity.java
+++ app/src/main/java/org/raspiot/raspiot/DeviceAdder/DeviceAdderActivity.java
@@ -1,14 +1,25 @@
 package org.raspiot.raspiot.DeviceAdder;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.acker.simplezxing.activity.CaptureActivity;
 
 import org.raspiot.raspiot.R;
 import org.raspiot.raspiot.JsonGlobal.ControlMessage;
@@ -37,6 +48,7 @@ public class DeviceAdderActivity extends AppCompatActivity {
     private final int ADD_DEVICE_SUCCEED = 1;
     private final int ADD_DEVICE_ERROR = -1;
     private final int NETWORK_ERROR = -2;
+    private static final int REQ_CODE_PERMISSION = 0x1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,7 +120,75 @@ public class DeviceAdderActivity extends AppCompatActivity {
     private void initUIWidget(){
         editDeviceUuid = (EditText) findViewById(R.id.edit_device_adder_device_uuid);
         editDeviceName = (EditText) findViewById(R.id.edit_device_adder_device_name);
+        Button btn = (Button) findViewById(R.id.btn_device_adder_qr_code);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //启动 扫描
+                if(ContextCompat.checkSelfPermission(DeviceAdderActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(DeviceAdderActivity.this, new String[]{Manifest.permission.CAMERA}, REQ_CODE_PERMISSION);
+                }else {
+                    startCaptureActivityForResult();
+                }
+            }
+        });
+    }
 
+    private void startCaptureActivityForResult() {
+        Intent intent = new Intent(DeviceAdderActivity.this, CaptureActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(CaptureActivity.KEY_NEED_BEEP, CaptureActivity.VALUE_BEEP);
+        bundle.putBoolean(CaptureActivity.KEY_NEED_VIBRATION, CaptureActivity.VALUE_VIBRATION);
+        bundle.putBoolean(CaptureActivity.KEY_NEED_EXPOSURE, CaptureActivity.VALUE_NO_EXPOSURE);
+        bundle.putByte(CaptureActivity.KEY_FLASHLIGHT_MODE, CaptureActivity.VALUE_FLASHLIGHT_OFF);
+        bundle.putByte(CaptureActivity.KEY_ORIENTATION_MODE, CaptureActivity.VALUE_ORIENTATION_AUTO);
+        bundle.putBoolean(CaptureActivity.KEY_SCAN_AREA_FULL_SCREEN, CaptureActivity.VALUE_SCAN_AREA_FULL_SCREEN);
+        bundle.putBoolean(CaptureActivity.KEY_NEED_SCAN_HINT_TEXT, CaptureActivity.VALUE_SCAN_HINT_TEXT);
+        intent.putExtra(CaptureActivity.EXTRA_SETTING_BUNDLE, bundle);
+        startActivityForResult(intent, CaptureActivity.REQ_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_CODE_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // User agree the permission
+                    startCaptureActivityForResult();
+                } else {
+                    // User disagree the permission
+                    Toast.makeText(this, "You must agree the camera permission request before you use the code scan function", Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CaptureActivity.REQ_CODE:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        try {
+                            String[] QRData = data.getStringExtra(CaptureActivity.EXTRA_SCAN_RESULT).split(";");
+                            editDeviceUuid.setText(QRData[0]);
+                            editDeviceName.setText(QRData[1]);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        break;
+                    case RESULT_CANCELED:
+                        if (data != null) {
+                            // for some reason camera is not working correctly
+                            ToastShowInBottom("Camera is not working correctly.");
+                        }
+                        break;
+                }
+                break;
+        }
     }
 
 
